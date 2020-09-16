5f3c4041f1043983485c69baad40a2e262a01797
POINT Chatchawan Singkaling <pit.chatchawan.si@glsict.com>
2020-09-16 21:40:52 +0700

    insert prefer directory for backup

--- up.sh
+++ up.sh
@@ -1,3 +1,4 @@
+mkdir -p $(pwd)/{vesta,home,backup} && \
 docker run -d --restart=always \
 -p 13322:22 -p 19080:80 -p 19443:443 -p 19088:8083 --name vestacp \
 -v $(pwd)/vesta:/vesta -v $(pwd)/home:/home -v $(pwd)/backup:/backup \
